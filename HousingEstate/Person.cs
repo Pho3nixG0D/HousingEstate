ed3914e7b710761468354784a88e3e32dd048721
Pho3nixG0D <oliverkrskagmail.com>
2022-12-07 23:47:51 +0100

    Add project files.

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingEstate
+{
+    internal class Person
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int Age { get; set; }
+
+        /// <summary>
+        /// Constructors for Person
+        /// </summary>
+        public Person(string fname, string lname, int age)
+        {
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($" First name: {FirstName}\n Last Name: {LastName}\n Age: {Age}\n");
+        }
+    }
+}
---
+++ HousingEstate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingEstate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person Lukas = new Person("Lukas","Cupa",15);
+
+            Console.WriteLine(Lukas.ToString());
+        }
+    }
+}
